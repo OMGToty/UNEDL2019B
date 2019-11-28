cbeb3dac0f54d40b95c6c22efaa938bed594a338
Hans Castillo Solis <bab2pacrixdimv3@hotmail.com>
2019-11-27 22:31:51 -0600

    Actualizacion 4.0

---
+++ Console/Herencia/Herencia/Herencia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Herencia
+    {
+        static void Main(string[] args) 
+        {
+            MadrePadre hijo = new MadrePadre();
+            hijo.Nombre = "Hans";
+            hijo.Apellido = "Castillo";
+            hijo.Edad = "17";
+            Console.WriteLine("El nombre del hijo es: "+hijo.Nombre);
+            Console.WriteLine("El apellido del hijo es: "+hijo.Apellido);
+            Console.WriteLine("la edad del hijo es: "+hijo.Edad);
+            hijo.Saltar_3_metros("Hans");
+            hijo.Cortarpelo("hans");
+
+        }
+    }
+}
---
+++ Console/Herencia/Herencia/MadrePadre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herencia
+{
+    public class MadrePadre
+    {
+        private string nombre;
+        private string apellido;
+        private string edad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Edad { get => edad; set => edad = value; }
+
+        public void Cortarpelo(String nombre)
+        {
+            Console.WriteLine("El muchacho" + nombre + " Aprendio a cortar a pelo gracias a su mother");
+        }
+        public void Saltar_3_metros(String nombre)
+        {
+            Console.WriteLine("El muchacho" +nombre+" Aprendio a saltar 3 metros gracias a su padre");
+        }
+
+    }
+}
